012d42f4197104c1a1bd3c0adfca68c63aa786ea
David Conrad <david.scott.conrad@gmail.com>
2025-10-06 18:18:40 -0700

    up to find index

---
+++ app/(Kambaz)/Labs/Lab3/FindFunction.tsx
@@ -0,0 +1,20 @@
+export default function FindFunction() {
+    let numberArray1 = [1, 2, 3, 4, 5];
+    let stringArray1 = ["string1", "string2", "string3"];
+    const four = numberArray1.find((a) => a === 4);
+    const string3 = stringArray1.find((a) => a === "string3");
+
+    const fourIndex = numberArray1.findIndex(a => a === 4);
+    const string3Index = stringArray1.findIndex(a => a === 'string3');
+    return (
+        <div id="wd-find-function">
+            <h4>Find Function</h4>
+            four = {four} <br />
+            string3 = {string3} <hr />
+
+            <h4>FindIndex Function</h4>
+            fourIndex = {fourIndex} <br />
+            string3Index = {string3Index} <br />
+        </div>
+    );
+}
--- app/(Kambaz)/Labs/Lab3/page.tsx
+++ app/(Kambaz)/Labs/Lab3/page.tsx
@@ -7,6 +7,13 @@ import ConditionalOutputIInline from "./ConditionalOutputInline";
 import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
 import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
 
 export default function Lab3() {
     return (
@@ -21,6 +28,13 @@ export default function Lab3() {
             <LegacyFunctions />
             <ArrowFunctions />
             <ImpliedReturn />
+            <TemplateLiterals />
+            <SimpleArrays />
+            <ArrayIndexAndLength />
+            <AddingAndRemovingToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <FindFunction />
         </div>
     );
 }
