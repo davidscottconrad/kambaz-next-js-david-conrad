f68d87393c553c460d8df205243279a76e070a7c
David Conrad <david.scott.conrad@gmail.com>
2025-10-06 18:10:57 -0700

    up to implied return

--- app/(Kambaz)/Labs/Lab3/page.tsx
+++ app/(Kambaz)/Labs/Lab3/page.tsx
@@ -1,15 +1,26 @@
-
+import VariablesAndConstants from "./VariablesAndConstants";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputIInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
 
 export default function Lab3() {
     return (
-
-
-
-
-        < div >
-
-            <h2>Lab 3</h2>
-            <h3>intro to typescript</h3>
-        </div >
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputIInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturn />
+        </div>
     );
 }
