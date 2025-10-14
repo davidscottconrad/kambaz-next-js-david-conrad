bfed504ae47194b6c8ebce3500a6f2c2d3f2543f
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-10-14 10:32:38 -0700

    add key for AddingandRemove function

--- app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
+++ app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
@@ -1,10 +1,13 @@
 export default function AddingAndRemovingToFromArrays() {
-    let numberArray1 = [1, 2, 3, 4, 5];
-    let stringArray1 = ["string1", "string2"];
-    let todoArray = [<li>Buy milk</li>, <li>Feed the pets</li>];
+    const numberArray1 = [1, 2, 3, 4, 5]; //never reassign the variables themselves—only mutate their contents—so they should be const.
+    const stringArray1 = ["string1", "string2"];
+    const todoArray = [<li key="buy-milk">Buy milk</li>,
+    <li key="feed-pets">Feed the pets</li>];
+
+    // mutate contents (const is fine; we're not reassigning the variables)
     numberArray1.push(6); // adding new items
     stringArray1.push("string3");
-    todoArray.push(<li>Walk the dogs</li>);
+    todoArray.push(<li key="walk-dogs">Walk the dogs</li>);
     numberArray1.splice(2, 1); // remove 1 item starting at 2
     stringArray1.splice(1, 1);
     return (
@@ -13,6 +16,10 @@ export default function AddingAndRemovingToFromArrays() {
             numberArray1 = {numberArray1} <br />
             stringArray1 = {stringArray1} <br />
             Todo list:
-            <ol>{todoArray}</ol><hr />
+            <ol>
+                {todoArray.map(t => (
+                    <li key={t.key}>{t}</li>
+                ))}
+            </ol><hr />
         </div>);
 }
