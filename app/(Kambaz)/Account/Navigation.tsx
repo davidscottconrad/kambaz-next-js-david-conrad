09f1740153e335731be3d42438db508e51a5d288
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-10-26 21:32:34 -0700

    fix account navigation

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -7,6 +7,24 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const pathname = usePathname();
+  return (
+    <ul className="list-group">
+      {links.map((link) => (
+        <li
+          key={link}
+          className={`list-group-item ${pathname.includes(link.toLowerCase()) ? "active" : ""}`}
+        >
+          {/* Link to the respective path (e.g., /Account/Profile) */}
+          <Link
+            href={`/Account/${link}`}
+            className="text-decoration-none"
+          >
+            {link}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 // import Link from "next/link";
 // export default function AccountNavigation() {
