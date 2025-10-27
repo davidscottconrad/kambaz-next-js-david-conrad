e47f6ee39e705982b71fba08ee622350e7cdda67
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-10-26 21:40:28 -0700

    add provider in lab layout

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,17 +1,22 @@
 import { ReactNode } from "react";
 import TOC from "./TOC";
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function LabsLayout({ children }:
   Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
+  return (
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
+  );
+}
