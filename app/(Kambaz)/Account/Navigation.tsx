fde0a822c9d02fac851fc38e9cd2a822472cdb32
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-10-26 20:39:25 -0700

    eslint

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Link from "next/link";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
