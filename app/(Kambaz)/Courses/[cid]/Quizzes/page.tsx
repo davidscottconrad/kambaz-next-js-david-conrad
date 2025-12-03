82c2f047ba3cba6559b3d40d53add8141400c563
Chunling Ji <chunlingji@Chunlings-KATE-MacBook-Pro.local>
2025-12-02 20:43:35 -0800

    no-explicit-any

--- app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import QuizzesControls from "./QuizzesControls";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { useParams } from "next/navigation";
